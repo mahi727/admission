edc795fd709e2c88ad797bf1970530dc2bcfbcff
Saiful <saiful.islam.0476@datasoft-bd.com>
2018-03-22 17:26:57 +0600

    my project

---
+++ application/views/Layout/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+
+    </div>
+    <strong>Copyright &copy; mahiburi</strong> All rights
+    reserved.
+</footer>
---
+++ application/views/Layout/master.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<style>
+    .wrapper {
+        background-color: white;
+    }
+</style>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dashboard</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <?php
+    $this->load->view('Layout/css');
+    ?>
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+    <?php
+    $this->load->view('Layout/header');
+    $this->load->view('Layout/leftMenu');
+    ?>
+    <!-- Left side column. contains the logo and sidebar -->
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Welcome to my homepage
+            </h1>
+        </section>
+
+        <!-- Main content -->
+        <?php
+/*        $this->load->view('Layout/content');*/
+        ?>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <?php
+    $this->load->view('Layout/footer');
+    ?>
+
+</div>
+<!-- ./wrapper -->
+    <?php
+    $this->load->view('Layout/footer');
+    ?>
+
+</body>
+</html>
